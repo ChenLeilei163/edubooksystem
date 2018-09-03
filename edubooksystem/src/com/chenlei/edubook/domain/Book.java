2761e54027363454368766d056bc4dcd3d29ea13
lanou3g <lanou3g@LANOU-20170629H>
2018-09-03 10:59:48 +0800

    测试java文件上传

--- edubooksystem/src/com/chenlei/edubook/domain/Book.java
+++ edubooksystem/src/com/chenlei/edubook/domain/Book.java
@@ -5,4 +5,28 @@ public class Book {
 	private String bookname;
 	private String bookimg;
 	private double bookprice;
+	public String getBookid() {
+		return bookid;
+	}
+	public void setBookid(String bookid) {
+		this.bookid = bookid;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getBookimg() {
+		return bookimg;
+	}
+	public void setBookimg(String bookimg) {
+		this.bookimg = bookimg;
+	}
+	public double getBookprice() {
+		return bookprice;
+	}
+	public void setBookprice(double bookprice) {
+		this.bookprice = bookprice;
+	}
 }
